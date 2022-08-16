67f2fab49e897cc6fa0f7112fe8a6924d60851d5
siddh <siddh@Siddhi>
2022-08-16 16:28:51 +0530

    This is a trial.

---
+++ Trial/src/Introduction.java
@@ -0,0 +1,8 @@
+
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println("Hello there! My name is Siddhi Pandey"
+				+ "I am an Engineering student"
+				+ "I am interested in learning new technical skills");
+	}
+}
